d4fe43ab7e6e5a3e47caf2a9f658f64f06709df5
Dimas <dimas11@rocketmail.com>
2013-02-22 02:19:34 +0700

    Kategori baru udah pop up

--- src/DashboardPage.js
+++ src/DashboardPage.js
@@ -3,5 +3,17 @@
  */
 
 function fitur() {
-	alert("maaf situs sedang dalam pengembangan");
+	myWindow=window.open("KategoriBaru.html","",'width=400,height=300');
+	//myWindow.document.write("<p>This is 'myWindow'</p>");
+	myWindow.focus();
+	//alert(".");
+	
+	//document.getElementById('newkategori').innerHTML="Nama Kategori";
+	
+	//alert("maaf situs sedang dalam pengembangan");
+}
+
+function tutup() {
+	alert("kategori dibuat namun belum disimpan");
+	window.close();
 }
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -14,162 +14,124 @@ function validation (form) {
  	}
  }
 
-function changeLink()
-{
-	document.getElementById('reg').innerHTML="BAZINGA! You can register";
- }
 
-function regsukses() {
+/*function regsukses() {
 	alert("Registrasi Berhasil");
-}
-
-/*function formValidation()
-{
-var uid = document.registration.userid;
-var passid = document.registration.passid;
-var uname = document.registration.username;
-var uadd = document.registration.address;
-var ucountry = document.registration.country;
-var uzip = document.registration.zip;
-var uemail = document.registration.email;
-var umsex = document.registration.msex;
-var ufsex = document.registration.fsex; if(userid_validation(uid,5,12))
-{
-if(passid_validation(passid,7,12))
-{
-if(allLetter(uname))
-{
-if(alphanumeric(uadd))
-{ 
-if(countryselect(ucountry))
-{
-if(allnumeric(uzip))
-{
-if(ValidateEmail(uemail))
-{
-if(validsex(umsex,ufsex))
-{
-}
-} 
-}
-} 
-}
-}
-}
-}
-return false;
-
-} function userid_validation(uid,mx,my)
-{
-var uid_len = uid.value.length;
-if (uid_len == 0 || uid_len >= my || uid_len < mx)
-{
-alert("User Id should not be empty / length be between "+mx+" to "+my);
-uid.focus();
-return false;
-}
-return true;
-}
-function passid_validation(passid,mx,my)
-{
-var passid_len = passid.value.length;
-if (passid_len == 0 ||passid_len >= my || passid_len < mx)
-{
-alert("Password should not be empty / length be between "+mx+" to "+my);
-passid.focus();
-return false;
-}
-return true;
-}
-function allLetter(uname)
-{ 
-var letters = /^[A-Za-z]+$/;
-if(uname.value.match(letters))
-{
-return true;
-}
-else
-{
-alert('Username must have alphabet characters only');
-uname.focus();
-return false;
-}
-}
-function alphanumeric(uadd)
-{ 
-var letters = /^[0-9a-zA-Z]+$/;
-if(uadd.value.match(letters))
-{
-return true;
-}
-else
-{
-alert('User address must have alphanumeric characters only');
-uadd.focus();
-return false;
-}
-}
-function countryselect(ucountry)
-{
-if(ucountry.value == "Default")
-{
-alert('Select your country from the list');
-ucountry.focus();
-return false;
-}
-else
-{
-return true;
-}
-}
-function allnumeric(uzip)
-{ 
-var numbers = /^[0-9]+$/;
-if(uzip.value.match(numbers))
-{
-return true;
-}
-else
-{
-alert('ZIP code must have numeric characters only');
-uzip.focus();
-return false;
-}
-}
-function ValidateEmail(uemail)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(uemail.value.match(mailformat))
-{
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-uemail.focus();
-return false;
-}
-} function validsex(umsex,ufsex)
-{
-x=0;
-
-if(umsex.checked) 
-{
-x++;
-} if(ufsex.checked)
-{
-x++; 
-}
-if(x==0)
-{
-alert('Select Male/Female');
-umsex.focus();
-return false;
-}
-else
-{
-alert('Form Succesfully Submitted');
-window.location.reload()
-return true;
-}
 } */
+
+/* function formValidation(form)
+{
+	var passid = form.password.value;
+	var conf = form.confpassword.value;
+	var uname = form.nama.value;
+	var uemail = form.usermail.value;
+	 
+	 if(ValidateEmail(uemail))
+	
+		{
+			if(passid_validation(passid))
+				{
+					if(conf_validation(passid,conf))
+						{
+							if(allLetter(uname))
+								{
+		 							
+								}
+						} 
+				}
+		} 
+								
+		return false;
+} */
+
+
+	function passid_validation(passid)
+	{
+		var passid = form.password.value;
+		var passid_len = passid.length;
+		if (passid_len == 0 )
+		{
+			alert("Password should not be empty !");
+			passid.focus();
+			return false;
+		}
+		return true;
+	}
+
+/*
+function conf_validation(passid,conf)
+	{
+		//var passid_len = passid.value.length;
+		if (passid != conf )
+		{
+			alert("Password should not be empty !");
+			passid.focus();
+			return false;
+		}
+		return true;
+	}
+
+	function allLetter(uname)
+	{ 
+		var letters = /^[A-Za-z]+$/;
+		if(uname.value.match(letters))
+		{
+			alert('Form Succesfully Submitted');
+				//window.location.reload()
+			return true;
+		}
+		else
+		{
+			alert('Jangan alay dech namanya');
+			uname.focus();
+			return false;
+		}
+	}
+
+	
+
+	
+*/
+	
+	/*function ValidateEmail(uemail)
+	{
+		var uemail = form.usermail.value;
+		var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		if(uemail.value.match(mailformat))
+			{
+				alert("halo");
+				return true;
+			}
+		else
+			{
+				alert("You have entered an invalid email address!");
+				//uemail.focus();
+				return false;
+			}
+	} 
+ */
+/*	function validsex(umsex,ufsex)
+	{
+		x=0;
+
+		if(umsex.checked) 
+			{
+				x++;
+			} 
+		if(ufsex.checked)
+			{
+				x++; 
+			}
+		if(x==0)
+			{
+				alert('Select Male/Female');
+				umsex.focus();
+				return false;
+			}
+		else
+			{
+				alert('Form Succesfully Submitted');
+				window.location.reload()
+				return true;
+			}
+		} */
