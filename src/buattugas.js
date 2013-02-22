896a37d73f2b5682216100c48e77943d900fa67a
Nurul Fithria Lubis <nfithrialubis@gmail.com>
2013-02-22 18:59:58 +0700

    limitasi tipe input file

--- src/buattugas.js
+++ src/buattugas.js
@@ -2,6 +2,7 @@
 function validateForm(){
 	if (document.getElementById("namatugas").value==="") {
 	 	alert("Input masih salah!");
+
 	} 
 	else {
 		window.location='rinciantugas.html';
@@ -9,5 +10,25 @@ function validateForm(){
 }
 
 function cancel() {
-	window.location=rinciantugas.html;
+	window.location='rinciantugas.html';
 }
+
+function seperateTags(){
+	var tag = document.getElementById("inputtags");
+	var ArrayofTags = tag.split(',');
+}
+
+function checkFile() {
+        var fileElement = document.getElementById("inputattachment");
+        var fileExtension = "";
+        if (fileElement.value.lastIndexOf(".") > 0) {
+            fileExtension = fileElement.value.substring(fileElement.value.lastIndexOf(".") + 1, fileElement.value.length);
+        }
+        if (fileExtension == "gif" || fileExtension == "jpg" || fileExtension=="mp3" || fileExtension=="txt") {
+            return true;
+        }
+        else {
+            alert("Unsupported file type! Please select mp3, gif, jpg, and txt files only");
+            return false;
+        }
+    }
