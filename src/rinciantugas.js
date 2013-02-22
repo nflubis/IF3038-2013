acc465579a5a37ba6e10b35fc14977690d143544
Dimas <dimas11@rocketmail.com>
2013-02-22 16:58:02 +0700

    Attachments
    Baru bisa tipe foto

--- src/rinciantugas.js
+++ src/rinciantugas.js
@@ -1,4 +1,5 @@
 function editTask(){
+<<<<<<< HEAD
 	var dead = document.getElementById("atributdeadline");
 	var ass = document.getElementById("atributassignee");
 	var tag = document.getElementById("atributtag");
@@ -9,3 +10,89 @@ function editTask(){
 	tag.innerHTML="<input type='tag'>";
 	button.value="Save Changes";
 }
+=======
+		
+}
+
+/**
+ * @author Dimas
+ */
+
+
+        function ShowImagePreview( files )
+{
+    if( !( window.File && window.FileReader && window.FileList && window.Blob ) )
+    {
+      alert('The File APIs are not fully supported in this browser.');
+      return false;
+    }
+
+    if( typeof FileReader === "undefined" )
+    {
+        alert( "Filereader undefined!" );
+        return false;
+    }
+
+    var file = files[0];
+
+    if( !( /image/i ).test( file.type ) )
+    {
+        alert( "File is not an image." );
+        return false;
+    }
+
+    reader = new FileReader();
+    reader.onload = function(event) 
+            { var img = new Image; 
+              img.onload = UpdatePreviewCanvas; 
+              img.src = event.target.result;  }
+    reader.readAsDataURL( file );
+}   
+
+function UpdatePreviewCanvas()
+{
+    var img = this;
+    var canvas = document.getElementById( 'previewcanvas' );
+
+    if( typeof canvas === "undefined" 
+        || typeof canvas.getContext === "undefined" )
+        return;
+
+    var context = canvas.getContext( '2d' );
+
+    var world = new Object();
+    world.width = canvas.offsetWidth;
+    world.height = canvas.offsetHeight;
+
+    canvas.width = world.width;
+    canvas.height = world.height;
+
+    if( typeof img === "undefined" )
+        return;
+
+    var WidthDif = img.width - world.width;
+    var HeightDif = img.height - world.height;
+
+    var Scale = 0.0;
+    if( WidthDif > HeightDif )
+    {
+        Scale = world.width / img.width;
+    }
+    else
+    {
+        Scale = world.height / img.height;
+    }
+    if( Scale > 1 )
+        Scale = 1;
+
+    var UseWidth = Math.floor( img.width * Scale );
+    var UseHeight = Math.floor( img.height * Scale );
+
+    var x = Math.floor( ( world.width - UseWidth ) / 2 );
+    var y = Math.floor( ( world.height - UseHeight ) / 2 );
+
+    context.drawImage( img, x, y, UseWidth, UseHeight );  
+}
+ 
+    
+>>>>>>> Attachments
